994704beb486b2aee5e35bd0a8685acacc41caac
Adam Barker <git@q-state.co.uk>
2014-03-25 13:04:12 +0000

    Added new methods and tests

--- spec/getRhymes-spec.js
+++ spec/getRhymes-spec.js
@@ -4,7 +4,7 @@ describe("tests for getRhymes", function() {
 
     console.log('rbM is ', rbModule);
 
-    it("returns some data", function(done) {
+    xit("returns some data", function(done) {
         return rbModule.getRhymes('train')
             .then(function (d) {
                 expect(d).toNotBe(null);
@@ -12,7 +12,7 @@ describe("tests for getRhymes", function() {
             });
     });
 
-    it("returns 10 records", function(done) {
+    xit("returns 10 records", function(done) {
         return rbModule.getRhymes('train', 0, 10)
             .then(function (d) {
                 expect(d.length).toBe(10);
@@ -20,7 +20,7 @@ describe("tests for getRhymes", function() {
             });
     });
 
-    it("returns 3 syllable words", function(done) {
+    xit("returns 3 syllable words", function(done) {
         return rbModule.getRhymes('train', 3)
             .then(function (d) {
                 expect(d.length).toBeGreaterThan(0);
@@ -31,6 +31,46 @@ describe("tests for getRhymes", function() {
                     expect(firstWord.syllables).toBe('3');
                     done();
                 }
+            });
+    });
+
+    xit("gets information about a word", function(done) {
+        return rbModule
+            .getWord('indestructable')
+            .then(function (d) {
+                console.log(d);
+                expect(d).toNotBe(null);
+               done();
+            });
+    });
+
+    xit("'information' has four syllables", function(done) {
+        return rbModule
+            .getWord('information')
+            .then(function (d) {
+                console.log(d);
+                expect(d.syllables).toBe('4');
+                done();
+            });
+    });
+
+    xit("'cat' returns some portmanteau's", function(done) {
+        return rbModule
+            .getPortmanteaus('cat')
+            .then(function (d) {
+                console.log(d);
+                expect(d.length).toBeGreaterThan(0);
+                done();
+            });
+    });
+
+    it("'cat' with maxResults of ten returns ten portmanteau's", function(done) {
+        return rbModule
+            .getPortmanteaus('cat', 10)
+            .then(function (d) {
+                console.log(d);
+                expect(d.length).toBe(10);
+                done();
             });
     });
 });
